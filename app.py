3655b3f3b465aa2cf233790b7fbb3d534327886e
malo21st <44739330+malo21st@users.noreply.github.com>
2023-05-25 19:31:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -37,9 +37,8 @@ user_input = st.text_input("メッセージを入力してください。", key=
 
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
-    for message in reversed(messages):  # 直近のメッセージを上に
-#     for message in reversed(messages[1:]):  # 直近のメッセージを上に
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
         if message["role"]=="assistant":
-            st.warning(message["content"])
-        else:
+            st.success(message["content"])
+        elif message["role"]=="user":
             st.info(message["content"])
