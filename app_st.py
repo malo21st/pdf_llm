2bb8897a97ca4abac303b92e6125326250408ebf
malo21st <44739330+malo21st@users.noreply.github.com>
2023-05-26 16:03:41 +0900

    Update app_st.py

--- app_st.py
+++ app_st.py
@@ -46,7 +46,6 @@ def store_del_msg():
 # ユーザーインターフェイスの構築
 st.sidebar.title("補助金さん")
 st.sidebar.write("補助金・助成金についてお任せあれ")
-st.sidebar.image(image, caption='展示会出展助成事業', width=150)
 
 if st.session_state["qa"]:
     messages = st.session_state["qa"]
@@ -57,6 +56,7 @@ if st.session_state["qa"]:
             st.success(message["msg"])
 
 user_input = st.sidebar.text_input("ご質問をどうぞ", key="user_input", on_change=store_del_msg)
+st.sidebar.image(image, caption='展示会出展助成事業', width=150)
 # here is the key, setup a empty container first
 chat_box=st.empty() 
 stream_handler = StreamHandler(chat_box)
