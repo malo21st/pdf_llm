535b97a77b4192cd4fd6362c1622fb30917a4acf
malo21st <44739330+malo21st@users.noreply.github.com>
2023-05-26 11:54:16 +0900

    Update app_st.py

--- app_st.py
+++ app_st.py
@@ -11,23 +11,14 @@ class StreamHandler(BaseCallbackHandler):
         self.container = container
         self.text=initial_text
     def on_llm_new_token(self, token: str, **kwargs) -> None:
-        # "/" is a marker to show difference 
-        # you don't need it 
-        self.text+=token+"/" 
-        self.container.markdown(self.text) 
+        self.text+=token 
+        self.container.info(self.text) 
 
 query=st.text_input("input your query",value="Tell me a joke")
-ask_button=st.button("ask") 
+# ask_button=st.button("ask") 
 
 st.markdown("### streaming box")
 # here is the key, setup a empty container first
 chat_box=st.empty() 
 stream_handler = StreamHandler(chat_box)
-chat = ChatOpenAI(max_tokens=25, streaming=True, callbacks=[stream_handler])
-
-st.markdown("### together box")  
-
-if query and ask_button: 
-    response = chat([HumanMessage(content=query)])    
-    llm_response = response.content  
-    st.markdown(llm_response)
+chat = ChatOpenAI(streaming=True, callbacks=[stream_handler])
