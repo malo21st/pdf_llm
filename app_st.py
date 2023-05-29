0c4fd09478de2d594c3954df8e1ee8cb91eadd65
malo21st <44739330+malo21st@users.noreply.github.com>
2023-05-29 10:47:32 +0900

    Update app_st.py

--- app_st.py
+++ app_st.py
@@ -10,7 +10,7 @@ import os
 
 os.environ["OPENAI_API_KEY"] = st.secrets.openai_api_key
 
-INTRO = "概要を教えてください。回答後は改行して、「ご質問をどうぞ。」を付けて下さい。"
+INTRO = "概要を教えてください。回答の最後は、改行して「ご質問をどうぞ。」を付けて下さい。"
 if "qa" not in st.session_state:
     st.session_state["qa"] = [{"role": "Q", "msg": INTRO}]
 
